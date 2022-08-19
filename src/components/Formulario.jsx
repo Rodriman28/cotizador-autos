2fd5656101eb621c1adae3bb97bc88364277720f
Rodrigo <rodri.rirs@gmail.com>
2022-08-19 18:36:43 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,60 @@
+import { Fragment } from "react";
+import { MARCAS, YEARS, PLANES } from "../constants";
+
+const Formulario = () => {
+  return (
+    <>
+      <form>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Marca
+          </label>
+          <select
+            name="marca"
+            className="w-full p3 bg-white border border-gray-200"
+          >
+            <option value="">-- Selecciona Marca --</option>
+            {MARCAS.map((marca) => (
+              <option key={marca.id} value={marca.id}>
+                {marca.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Año
+          </label>
+          <select
+            name="marca"
+            className="w-full p3 bg-white border border-gray-200"
+          >
+            <option value="">-- Selecciona Año --</option>
+            {YEARS.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Elije un plan
+          </label>
+          <div className="flex gap-3">
+            {PLANES.map((plan) => (
+              <Fragment key={plan.id}>
+                <label>
+                  {plan.nombre}
+                  <input type="radio" name="plan" value={plan.id} />
+                </label>
+              </Fragment>
+            ))}
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
